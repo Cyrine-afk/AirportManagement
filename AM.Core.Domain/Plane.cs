1ded59a683cc7cab6850e20f9e9ec11697eaf3ce
Cyrine-afk <cyrine.trabelsi@esprit.tn>
2022-11-07 15:06:16 +0100

    Annotations Plane Class

--- AM.Core.Domain/Plane.cs
+++ AM.Core.Domain/Plane.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,13 @@ namespace AM.Core.Domain
     }
     public class Plane
     {
+        [Range(0, int.MaxValue, ErrorMessage = "Error, Positive Integer")] //capacity est un entier positif
         public int Capacity { get; set; }
+
         public DateTime ManifactureDate { get; set; }
+
         public int PlaneId { get; set; }
+        
         public PlaneType MyPlaneType { get; set; }
 
         public ICollection<Flight> Flights { get; set; }
